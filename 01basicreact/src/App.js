59e4303e69304e268950c4d804710efc7d2fb4a2
sakshiis-code <sakshihihisharma@gmail.com>
2025-12-21 11:14:30 +0530

    Understand the react flow and structure

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,13 @@
+import Chai from './Chai'
+
+function App() {
+  return (
+    <>
+    <Chai />
+    <h1>chai aur react | Sakshi Sharma | In this lecture, I've learned about how to run and create a basic react file with the help of "npx create-react-app app-name", also got a better understanding of package.json and other files. And, how to run and create a react folder using vite + javascript using "npm create vite@latest" command.</h1>
+    </>
+      
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from './chai'
+
+function App() {
+
+  return (
+    <>
+      <Chai />
+      <h1>Hiee, I am learning React</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus, autem!</p>
+    </>
+  )
+}
+
+export default App
