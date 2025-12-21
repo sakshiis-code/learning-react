ea14c22c854d13fe08a0b300594124ec17eb9474
sakshiis-code <sakshihihisharma@gmail.com>
2025-12-21 12:05:43 +0530

    Create your own React library

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,12 @@
 import Chai from './chai'
 
 function App() {
+  const username = "chai aur code"
 
   return (
     <>
       <Chai />
-      <h1>Hiee, I am learning React</h1>
+      <h1>Hiee, I am learning React {username}</h1>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus, autem!</p>
     </>
   )
