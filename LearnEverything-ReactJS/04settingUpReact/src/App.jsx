e127ddb6a4f33816294371d505856eee4f174767
sakshiis-code <sakshihihisharma@gmail.com>
2025-12-23 09:12:32 +0530

    Setting up React with Vite

---
+++ LearnEverything-ReactJS/04settingUpReact/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div id='parent'>
+        <h1 id='child1'>Hello guys</h1>
+        <h2 id='child2'>I'm learning React</h2>
+      </div>
+      <h4>This is interesting</h4>
+    </>
+  )
+}
+
+export default App
