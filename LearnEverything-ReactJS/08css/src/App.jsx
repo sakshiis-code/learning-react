9fffe0a3e0be173251e9777fa5ebb935d9252958
sakshiis-code <sakshihihisharma@gmail.com>
2025-12-26 08:03:46 +0530

    Module Css

---
+++ LearnEverything-ReactJS/08css/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from './components/button/Button.jsx'
+import Header from './components/header/Header.jsx'
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Button />
+    </div>
+  )
+}
+
+export default App
