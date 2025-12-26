703fb8384fae3d0c1e055f2914ffa50a730d026f
sakshiis-code <sakshihihisharma@gmail.com>
2025-12-26 08:24:31 +0530

    Tailwind CSS

---
+++ LearnEverything-ReactJS/09tailwindCSS/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen w-full bg-gray-800 text-center text-white py-40 font-semibold text-[30px]'>
+      App
+    </div>
+  )
+}
+
+export default App
