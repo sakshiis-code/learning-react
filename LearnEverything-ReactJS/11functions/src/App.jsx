c981cff8f472a634cc68fbdd5217bc71dbc99cc0
sakshiis-code <sakshihihisharma@gmail.com>
2025-12-30 08:28:02 +0530

    Working with Functions

---
+++ LearnEverything-ReactJS/11functions/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const App = () => {
+
+  const pageScrolling = (elem) => {
+    if (elem > 0) {
+      console.log('seedha scrolling');
+    } else {
+      console.log('ulta scrolling');
+      
+    }
+  } 
+
+  return (
+    <div onScroll={(elem) => {
+      pageScrolling(elem.deltaY)
+    }}>
+      <div className="page1"></div>
+      <div className="page2"></div>
+      <div className="page3"></div>
+    </div>
+  )
+}
+
+export default App
